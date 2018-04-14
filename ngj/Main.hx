e480e4e41e699899b6aeaf7e539be70d6caf73de
work-usb\selus <pupa60@o2.pl>
2018-04-14 12:07:26 +0200

    sound sample, works with haxe 3.4.7

--- ngj/Main.hx
+++ ngj/Main.hx
@@ -2,6 +2,7 @@ package ngj;
 import h2d.Bitmap;
 import h2d.Tile;
 import hxd.App;
+import hxd.res.Sound;
 import ngj.ui.SimpleUI;
 /**
  * ...
@@ -10,15 +11,13 @@ import ngj.ui.SimpleUI;
 class Main extends App
 {
 	var bmp:Bitmap;
-	var fui : h2d.Flow;
-	
-	var simpleUI:SimpleUI;
-	
+	var fui : h2d.Flow; //for UI
+
 	override function init() 
 	{
 		hxd.Res.initEmbed();
 		
-		var tile:Tile = hxd.Res.testLogo.toTile();
+		var tile:Tile = hxd.Res.testLogo.toTile(); //ładowanie png
 		tile = tile.center();
 		bmp = new h2d.Bitmap(tile, s2d);
 		bmp.x = s2d.width * 0.5;
@@ -28,9 +27,9 @@ class Main extends App
 		fui.isVertical = true;
 		fui.verticalSpacing = 5;
 		fui.padding = 10;
-		simpleUI = new SimpleUI(fui);
+		var simpleUI = new SimpleUI(fui);
 		simpleUI.addText("nie twierdze, ze to ma jakis duzy sens ale costam sie wyswietla");
-		simpleUI.addButton("tu nacisnij", function() {simpleUI.addCheck("uiuiuiuiuiuiui", function() {return true; }, function(b:Bool) { }); });
+		simpleUI.addButton("tu nacisnij", function() {hxd.Res.push.play(); });
 	}
 	
 	override function update(dt:Float) 
